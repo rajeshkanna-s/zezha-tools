e21d66e851b8082616a4aa7b656028f00d26cf33
RAJESHKANNA S <104251676+rajeshkanna-s@users.noreply.github.com>
2025-05-19 12:36:01 +0530

    Update scheduler.ts

--- scheduler.ts
+++ scheduler.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { exec } from 'child_process';
 
 // Run your automation script at 11:20, 11:24, and 11:28 AM
-cron.schedule('26,28,30 12 * * *', () => {
+cron.schedule('40,44,48 12 * * *', () => {
     console.log('⏰ Triggering automation at', new Date().toLocaleTimeString());
 
     exec('node --no-warnings -r dotenv/config src/scripts/dailyLogin.ts', (error, stdout, stderr) => {
